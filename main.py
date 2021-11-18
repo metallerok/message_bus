0336553c13b174c89b8d76bbde8f0f821b647316
Kireev Georgiy <metallerok@gmail.com>
2021-11-18 12:13:03 +0300

    message bus commands handle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from message_bus import MessageBus, Event, HandlerABC
-from typing import List
+from message_bus import MessageBus, Event, EventHandlerABC, Command, CommandHandlerABC
 from dataclasses import dataclass
 
 
@@ -13,29 +12,58 @@ class GoodbyeEvent(Event):
     message: str
 
 
-class HelloHandler(HandlerABC):
-    def handle(self, event: HelloEvent, *args, **kwargs):
-        print(event.message)
+@dataclass
+class HelloCommand(Command):
+    message: str
 
-        self.emmit_event(
+
+@dataclass
+class GoodbyeCommand(Command):
+    message: str
+
+
+class HelloEventHandler(EventHandlerABC):
+    def handle(self, event: HelloEvent, *args, **kwargs):
+        print(f"event: {event.message}")
+
+        self.emmit_message(
             GoodbyeEvent(message="Goodbye, word!")
         )
 
 
-class GoodByeHandler(HandlerABC):
+class GoodByeEventHandler(EventHandlerABC):
     def handle(self, event: HelloEvent, *args, **kwargs):
-        print(event.message)
+        print(f"event: {event.message}")
+
+
+class HelloCommandHandler(CommandHandlerABC):
+    def handle(self, cmd: HelloEvent, *args, **kwargs):
+        print(f"command: {cmd.message}")
+
+        self.emmit_message(
+            GoodbyeEvent(message="Goodbye, word!"),
+        )
+
+        self.emmit_message(
+            GoodbyeCommand(message="Goodbye, world")
+        )
+
+
+class GoodbyeCommandHandler(CommandHandlerABC):
+    def handle(self, cmd: GoodbyeCommand, *args, **kwargs):
+        print(f"command: {cmd.message}")
 
 
 if __name__ == '__main__':
     message_bus = MessageBus()
-    message_bus.set_handlers(
+    message_bus.set_event_handlers(
         event=HelloEvent,
-        handlers=[HelloHandler(), lambda event: print(f"from method: {event.message}")]
+        handlers=[HelloEventHandler(), lambda event: print(f"event from method: {event.message}")]
     )
-    message_bus.set_handlers(
-        event=GoodbyeEvent,
-        handlers=[GoodByeHandler()]
-    )
+    message_bus.set_event_handlers(event=GoodbyeEvent, handlers=[GoodByeEventHandler()])
+    message_bus.set_command_handler(cmd=HelloCommand, handler=HelloCommandHandler())
+    message_bus.set_command_handler(cmd=GoodbyeCommand, handler=GoodbyeCommandHandler())
 
-    message_bus.handle(HelloEvent(message="hello_world"))
+    message_bus.handle(HelloEvent(message="hello, world"))
+    print()
+    message_bus.handle(HelloCommand(message="hello, world"))
