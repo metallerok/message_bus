31fc3a7d4bc64a4bf1477e7c8ffa52c440ca4f13
Georgiy <metallerok@gmail.com>
2024-05-05 16:57:45 +0300

    inc

--- src/message_bus/repositories/outbox.py
+++ src/message_bus/repositories/outbox.py
@@ -1,18 +1,22 @@
 import abc
 import datetime as dt
-from dataclasses import dataclass
+from typing import List
 from message_bus.types import Message
 
 
-@dataclass
-class OutBoxMessageABC(abc.ABC):
-    id: str
-    type: str
-    message: Message
-    processed: dt.datetime
-
-
 class OutBoxRepoABC(abc.ABC):
+    def _add(self, outbox_message):
+        assert hasattr(outbox_message, "id")
+        assert hasattr(outbox_message, "type")
+        assert hasattr(outbox_message, "message") and type(getattr(outbox_message, "message")) == Message
+
+        self.add(outbox_message)
+
+
     @abc.abstractmethod
-    def add(self, outbox_message: OutBoxMessageABC):
+    def add(self, outbox_message):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def list_unprocessed(self) -> List:
         raise NotImplementedError
