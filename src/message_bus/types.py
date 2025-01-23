23f2a6f65a169d493f1b53c9653fca9d109c17a1
Georgiy <metallerok@gmail.com>
2025-01-23 17:07:50 +0300

    update serializators

--- src/message_bus/types.py
+++ src/message_bus/types.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import uuid
 from dataclasses_serialization.json import JSONSerializer
 
@@ -5,6 +6,12 @@ from dataclasses_serialization.json import JSONSerializer
 serializer = JSONSerializer
 serializer.serialization_functions[uuid.UUID] = lambda uuid_: str(uuid_)
 serializer.deserialization_functions[uuid.UUID] = lambda _, uuid_: uuid.UUID(uuid_)
+
+serializer.serialization_functions[dt.date] = lambda x: x.isoformat()
+serializer.deserialization_functions[dt.date] = lambda _, x: dt.date.fromisoformat(x)
+
+serializer.serialization_functions[dt.datetime] = lambda x: x.isoformat()
+serializer.deserialization_functions[dt.datetime] = lambda _, x: dt.datetime.fromisoformat(x)
 
 
 class Message:
